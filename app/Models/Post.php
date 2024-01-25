aa03e8550996cf1cd88baaa62952f854607c84ec
kanaraki-creater <KanAraki49@gmail.com>
2024-01-25 14:52:28 +0900

    投稿画面のバリデーションまで作成しました。

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -14,7 +14,7 @@ class Post extends Model
         'body',
     ];
     
-    public function getPaginateByLimit(int $limit_count = 10)
+    public function getPaginateByLimit(int $limit_count = 2)
     {
         return $this->orderBy('updated_at', 'DESC')->paginate($limit_count);
     }

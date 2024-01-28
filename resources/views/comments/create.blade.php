cfb23ce9dee3123999a0e1253bdb4a3cf4a29377
kanaraki-creater <KanAraki49@gmail.com>
2024-01-29 00:55:07 +0900

    コメント機能実装に取り掛かりました。

--- resources/views/comments/create.blade.php
+++ resources/views/comments/create.blade.php
@@ -2,26 +2,26 @@
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
     <head>
         <meta charset="utf-8">
-        <title>ライブを投稿する | ライブマップ</title>
+        <title>コメントを投稿する | ライブマップ</title>
     </head>
     <body>
         <h1>ライブマップ</h1>
         <form action="/posts" method="POST">
             @csrf
             <div class="title">
-                <h2>出演ライブ名・イベント名</h2>
-                <input type="text" name="post[title]" placeholder="ライブ名・イベント名"/>
+                <h2>コメントタイトル</h2>
+                <input type="text" name="post[title]" placeholder="最高でした！"/>
                 <p class="title_error">{{ $errors->first('post.title') }}</p>
             </div>
             <div class="body">
-                <h2>ライブ詳細</h2>
-                <textarea name="post[body]" placeholder="2024年１月１日に渋谷でワンマンライブをやります！"></textarea>
+                <h2>コメント本文</h2>
+                <textarea name="post[body]" placeholder="ダンスのレベルが高くてびっくりしました。"></textarea>
                 <p class="body_error">{{ $errors->first('post.body') }}</p>
             </div>
-            <input type="submit" value="投稿する"/>
+            <input type="submit" value="store"/>
         </form>
         <div class="footer">
-            <a href="/">戻る</a>
+            <a href="posts/{{ $post->id }}">戻る</a>
         </div>
     </body>
 </html>
